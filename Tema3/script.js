7af69b518f9397de1fb9770dd03452f96b207fd0
Florin <florin_ghiurca@yahoo.com>
2023-05-29 14:38:06 +0300

    Asta este a treia tema

---
+++ Tema3/script.js
@@ -0,0 +1,75 @@
+//Homework LOOPS 
+//1.De calculat media aritmetica a unui array de numere
+alert("Apasa ca sa incepem tema");
+// let sir =[1,2,4,5,23];
+// let medie=[(sir[0]+sir[1]+sir[2]+sir[3]+sir[4])/5];
+
+// console.log(sir);
+// console.log(medie);
+
+// let sir = [1, 2, 4, 5, 23];
+// let sum = 0;
+// let medie = 0;
+// for (let i = 0; i < sir.length; i++) {
+//     sum = sum + sir[i];
+// }
+// medie = sum / sir.length;
+// console.log(sir);
+// console.log(sum);
+// console.log(medie);
+
+
+//2.De calculat suma tuturor numerelor din array
+// let array = [1, 2, 4, true, false, 'Florin', 'Mihai', 15, 17];
+// let sum = 0;
+// for (let i = 0; i < array.length; i++) {
+//     if (typeof (array[i]) === typeof (1)) {
+//         sum = sum + array[i];
+//     }
+// }
+// console.log(array);
+// console.log(sum);
+
+//3.De afisat primele 20  de numere din sirul lui Fibonacci
+
+// let a = 0;
+// let b = 1;
+// console.log(a);
+// console.log(b);
+// let fibo = 0;
+// for (let i = 1; i < 20; i++) {
+//     fibo = a + b;
+//     a = b;
+//     b = fibo;
+//     console.log(fibo);
+// }
+
+
+
+
+
+
+
+
+
+//4.Primele 20 de numere din sirul lui fiboanaci doar ca sunt pare
+// let a = 0;
+// let b = 1;
+// console.log(a);
+// console.log(b);
+// let fibo = 0;
+// for (let i = 1; i < 20;) {
+//     fibo = a + b;
+//     a = b;
+//     b = fibo;
+//     // console.log(fibo);
+//     if (fibo % 2 === 0) {
+//         console.log(fibo);
+//         i = i + 1;
+//         console.log(i);
+
+//     }
+// }
+
+
+
